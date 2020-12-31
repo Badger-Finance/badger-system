f4d4912fd77c2fc109da846eb32ec559bb03dba0
dapp-whisperer <dapp-whisperer@pm.me>
2020-12-31 09:48:28 -0600

    add combined rewards script for testing

---
+++ scripts/rewards/rewards_combined.py
@@ -0,0 +1,29 @@
+from scripts.rewards.rewards_utils import fetch_rewards_preconditions
+import time
+
+from brownie import *
+from config.badger_config import badger_config
+from rich.console import Console
+from scripts.systems.badger_system import connect_badger
+
+from assistant.rewards.rewards_assistant import fetch_current_rewards_tree, run_action
+
+console = Console()
+
+
+def main():
+    badger = connect_badger(badger_config.prod_json, load_keeper=True, load_guardian=True)
+    (startBlock, endBlock) = fetch_rewards_preconditions(badger)
+
+    # If sufficient time has passed since last root proposal, propose a new root
+    rootProposed = run_action(
+        badger,
+        {"action": "rootUpdater", "startBlock": startBlock, "endBlock": endBlock},
+        test=False,
+    )
+
+    rootApproved = run_action(
+        badger,
+        {"action": "guardian", "startBlock": startBlock, "endBlock": endBlock},
+        test=False,
+    )
