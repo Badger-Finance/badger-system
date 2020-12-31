20e879af6901f4e1f9b1e219b32336268eaf3163
ren-zealot <0xrenzealot@gmail.com>
2020-12-31 01:16:13 -0500

    add ren adapter test

---
+++ run_js_tests.sh
@@ -0,0 +1,4 @@
+# Script for running js based tests since this repo contains mostly python based tests.
+
+# Target directories that contain tests here.
+./node_modules/.bin/mocha ./tests/ren-integration
