49dfa3ffc585968581e95591dccf3396312b6c40
ren-zealot <0xrenzealot@gmail.com>
2021-01-04 00:00:05 -0500

    Use hd wallet provider.

--- tests/ren-integration/adapter_test.js
+++ tests/ren-integration/adapter_test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const Web3 = require('web3');
 const RenJS = require('@renproject/ren');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 const { LogLevel } = require('@renproject/interfaces');
 const {
   SECONDS,
@@ -14,7 +15,11 @@ const logger = require('pino')({
 require('dotenv').config();
 
 const INFURA_PROJECT_ID = process.env.WEB3_INFURA_PROJECT_ID;
+const INFURA_URL = `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`;
+// Generate wallet from mnemonic.
+const MNEMONIC = process.env.TEST_MNEMONIC;
 const PRIVATE_KEY = process.env.TESTNET_PRIVATE_KEY;
+  // Ren test env (gateways) are deployed on the kovan testnet.
 const KOVAN_NETWORK_ID = 42;
 const KOVAN_BADGER_REN_ADAPTER_ADDR = '0x9C3Ef5ecE74D7D33D0f68dFb550D2474Dc8e9732';
 const MINUTES = 60 * SECONDS;
@@ -25,12 +30,11 @@ const renJS = new RenJS('testnet', {
 });
 let web3 = null;
 before(async () => {
-  // Need an infura account for testing.
-  // Ren test env (gateways) are deployed on the kovan testnet.
-  web3 = new Web3(`https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`);
   // Create and set default test account for all tests (we want to mint/burn from same eth addr).
-  const testAccount = web3.eth.accounts.create();
-  web3.eth.defaultAccount = testAccount.address;
+  const provider = new HDWalletProvider(MNEMONIC, INFURA_URL, 0, 10);
+  web3 = new Web3(provider);
+  const accounts = await web3.eth.getAccounts();
+  web3.eth.defaultAccount = accounts[0];  // use first derived path
   const networkID = await web3.eth.net.getId();
   if (networkID !== KOVAN_NETWORK_ID) {
     throw `Invalid network id ${networkID}, must use kovan network`;
@@ -53,7 +57,7 @@ describe('BadgerRenAdapter', function() {
         {
       	  name: '_to',
       	  type: 'address',
-      	  value: testAccount.address,
+      	  value: web3.eth.defaultAccount,
         },
       ],
       web3Provider: web3.currentProvider,
