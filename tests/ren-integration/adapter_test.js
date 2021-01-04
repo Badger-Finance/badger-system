e2f854db6ce4a7dc3979ff065ec9b956fba4726b
ren-zealot <0xrenzealot@gmail.com>
2021-01-03 22:32:51 -0500

    update tests.

--- tests/ren-integration/adapter_test.js
+++ tests/ren-integration/adapter_test.js
@@ -1,8 +1,15 @@
 const assert = require('assert');
 const Web3 = require('web3');
 const RenJS = require('@renproject/ren');
+const { LogLevel } = require('@renproject/interfaces');
+const {
+  SECONDS,
+  sleep,
+} = require('@renproject/utils');
 const CryptoAccount = require('send-crypto');
-const logger = require('pino')();
+const logger = require('pino')({
+    prettyPrint: { colorize: true }
+});
 
 require('dotenv').config();
 
@@ -10,9 +17,12 @@ const INFURA_PROJECT_ID = process.env.WEB3_INFURA_PROJECT_ID;
 const PRIVATE_KEY = process.env.TESTNET_PRIVATE_KEY;
 const KOVAN_NETWORK_ID = 42;
 const KOVAN_BADGER_REN_ADAPTER_ADDR = '0x9C3Ef5ecE74D7D33D0f68dFb550D2474Dc8e9732';
+const MINUTES = 60 * SECONDS;
 
 // initialize before running all tests
-const renJS = new RenJS('testnet');
+const renJS = new RenJS('testnet', {
+  //  logLevel: LogLevel.Debug
+});
 let web3 = null;
 before(async () => {
   // Need an infura account for testing.
@@ -24,39 +34,41 @@ before(async () => {
   }
 });
 
-describe('BadgerRenAdapter', () => {
+describe('BadgerRenAdapter', function() {
+  this.timeout(60 * MINUTES); // 60 minute t/o for integration tests
+
   it('should mint renBTC', async () => {
     const testAccount = web3.eth.accounts.create();
 
 	const mint = await renJS.lockAndMint({
-		// Send BTC from the Bitcoin blockchain to the Ethereum blockchain.
-		sendToken: RenJS.Tokens.BTC.Btc2Eth,
-		// The contract we want to interact with
-  		sendTo: KOVAN_BADGER_REN_ADAPTER_ADDR,
-  		contractFn: "mint",
-		// Arguments expected for calling `mint`
-  		contractParams: [
-		  {
-			  name: "_to",
-			  type: "address",
-			  value: testAccount.address,
-		  },
-		],
-  		web3Provider: web3.currentProvider,
+      // Send BTC from the Bitcoin blockchain to the Ethereum blockchain.
+      sendToken: RenJS.Tokens.BTC.Btc2Eth,
+      // The contract we want to interact with
+      sendTo: KOVAN_BADGER_REN_ADAPTER_ADDR,
+      contractFn: 'mint',
+      // Arguments expected for calling `mint`
+      contractParams: [
+        {
+      	  name: '_to',
+      	  type: 'address',
+      	  value: testAccount.address,
+        },
+      ],
+      web3Provider: web3.currentProvider,
 	});
 
     const gatewayAddress = await mint.gatewayAddress();
-    const account = new CryptoAccount(PRIVATE_KEY, { network: "testnet" });
+    const account = new CryptoAccount(PRIVATE_KEY, { network: 'testnet' });
     logger.info(
-        `BTC balance: ${await account.balanceOf(
-            "btc"
-        )} ${"btc"} (${await account.address("btc")})`
+      `BTC balance: ${await account.balanceOf(
+          'btc'
+      )} ${'btc'} (${await account.address('btc')})`
     );
 	const amount = RenJS.utils
-		.value(0.0008, "btc")
-		._smallest();
+	  .value(0.0011, 'btc')
+	  ._smallest();
     logger.info(`Sending BTC: ${amount}`);
-    await account.sendSats(gatewayAddress, amount, "btc");
+    await account.sendSats(gatewayAddress, amount, 'btc');
 
     // Submit mint
 	await submitMint(mint);
@@ -66,52 +78,60 @@ describe('BadgerRenAdapter', () => {
 // A debug `sleep`. It prints a count-down to the console.
 // tslint:disable-next-line: no-string-based-set-timeout
 const sleepWithCountdown = async (seconds) => {
-    while (seconds) {
-        process.stdout.write(`\u001b[0K\r${seconds}\r`);
-        await sleep(1 * SECONDS);
-        seconds -= 1;
-    }
-    process.stdout.write("\u001b[0K\r");
+  while (seconds) {
+    process.stdout.write(`\u001b[0K\r${seconds}\r`);
+    await sleep(1 * SECONDS);
+    seconds -= 1;
+  }
+  process.stdout.write('\u001b[0K\r');
 };
 
 const submitMint = async (mint) => {
-    // Wait for deposit to be received and submit to Lightnode + Ethereum.
-    const confirmations = 0;
-    logger.info(`Waiting for ${confirmations} confirmations...`);
+  const confirmations = 0;
+  logger.info(`Waiting for ${confirmations} confirmations...`);
 
-    const deposit = await mint
-        .wait(confirmations)
-        .on("deposit", (depositObject) => {
-            logger.info(
-                `Received a new deposit: ${JSON.stringify(depositObject)}`
-            );
-        });
+  let deposit;
+  try {
+    deposit = await mint
+      .wait(confirmations)
+      .on('deposit', (depositObject) => {
+        logger.info(
+          `Received a new deposit: ${JSON.stringify(depositObject)}`
+        );
+      });
+  } catch (e) {
+    console.error(`error waiting for deposit: ${e}`);
+    throw e;
+  }
 
-    await sleepWithCountdown(5);
+  await sleepWithCountdown(5);
 
-    logger.info(`Submitting deposit to RenVM...`);
-    const signature = await deposit
-        .submit()
-        .on("txHash", (txHash) => {
-            logger.debug(`Received txHash: ${txHash}`);
-            deposit.txHash().should.equal(txHash);
-        })
-        .on("status", (status) => {
-            logger.debug(`Received status: ${chalk.green(status)}`, {
-                overwrite: true,
-            });
-        });
+  logger.info(`Submitting deposit to RenVM...`);
+  const signature = await deposit
+    .submit()
+    .on('txHash', (txHashB64) => {
+        const buf = Buffer.from(txHashB64, 'base64');
+        const txHashHex = buf.toString('hex');
+      logger.info(`Received txHash(testnet.renVM): 0x${txHashHex}`);
+    })
+    .on('status', (status) => {
+      logger.info(`Received status: ${status}`, {
+        overwrite: true,
+      });
+    });
 
-    logger.info(`Submitting signature to Ethereum...`);
-    try {
-        await signature
-            .submitToEthereum(provider, { gas: 1000000 })
-            .on("eth_transactionHash", (txHash) => {
-                logger.debug(`Received txHash: ${txHash}`);
-            });
-        logger.info("Done waiting for Ethereum TX.");
-    } catch (error) {
-        logger.error(error);
-        throw error;
-    }
+  logger.info(`Submitting signature to Ethereum...`);
+  try {
+    await signature
+      .submitToEthereum(web3.currentProvider, { gas: 1000000 })
+      .on('eth_transactionHash', (txHashB64) => {
+        const buf = Buffer.from(txHashB64, 'base64');
+        const txHashHex = buf.toString('hex');
+        logger.info(`Received txHash(kovan.eth): 0x${txHashHex}`);
+      });
+    logger.info('Done waiting for Ethereum TX.');
+  } catch (error) {
+    logger.error(error);
+    throw error;
+  }
 };
