e50e8da290e851da395a1cdb980a4a5ac3dda4ad
darruma <umerderr@gmail.com>
2021-01-13 17:23:30 +0000

    feat: add sanity check for farm distribution in merkle tree

--- scripts/status/retroactive_farm_distribution.py
+++ scripts/status/retroactive_farm_distribution.py
@@ -8,6 +8,7 @@ from rich.console import Console
 from scripts.systems.badger_system import connect_badger
 
 from assistant.rewards.rewards_assistant import calc_harvest_meta_farm_rewards,process_cumulative_rewards,fetch_current_rewards_tree
+from assistant.rewards.rewards_checker import test_claims
 from assistant.subgraph.client import fetch_harvest_farm_events
 from assistant.rewards.RewardsList import RewardsList
 from config.rewards_config import rewards_config
@@ -20,14 +21,14 @@ console = Console()
 
 
 def main():
+    test = True
     badger = connect_badger(badger_config.prod_json)
     farmTokenAddress = "0xa0246c9032bC3A600820415aE600c6388619A14D"
     nextCycle = badger.badgerTree.currentCycle() + 1
-    console.log(nextCycle)
+    console.log("next cycle: {}".format(nextCycle))
     currentMerkleData = badger.badgerTree.getCurrentMerkleData()
     console.log(currentMerkleData)
     timeSinceLastUpdate = chain.time() - currentMerkleData[2]
-    
 
     print("Run at", int(time.time()))
 
@@ -37,7 +38,6 @@ def main():
     settStartBlock = 11376266
     startBlock = settStartBlock
     endBlock = int(harvestEvents[0]["blockNumber"])
-    console.log( sum ( [ int( h["farmToRewards"] ) for h in harvestEvents ] )/1e18)
     totalHarvested = 0
     for i in tqdm(range(len(harvestEvents))):
         console.log("Processing between {} and {}".format(startBlock,endBlock))
@@ -53,17 +53,16 @@ def main():
         totalShareSeconds = sum([u.shareSeconds for u in user_state])
         farmUnit = farmRewards/totalShareSeconds
         for user in user_state:
-            rewards.increase_user_rewards(user.address,farmTokenAddress,farmUnit * user.shareSeconds/1e18)
+            rewards.increase_user_rewards(user.address,farmTokenAddress,farmUnit * user.shareSeconds)
 
         if i+1 < len(harvestEvents):
             startBlock = int(harvestEvent["blockNumber"])
             endBlock = int(harvestEvents[i+1]["blockNumber"])
 
     console.log(rewards.claims)
-    console.log(sorted( [list(v.values())[0] for v in list(rewards.claims.values())  ] ))
-    console.log(totalHarvested)
+    console.log(sorted( [list(v.values())[0]/1e18 for v in list(rewards.claims.values())  ] ))
     claimsHarvested = sum( [list(v.values())[0] for v in list(rewards.claims.values())])
-    console.log(claimsHarvested)
+    console.log(claimsHarvested/1e18)
     
     currentRewards = fetch_current_rewards_tree(badger)
     cumulative_rewards = process_cumulative_rewards(currentRewards,rewards)
@@ -71,19 +70,36 @@ def main():
     merkleTree = rewards_to_merkle_tree(cumulative_rewards,settStartBlock,endBlock,{})
     # Upload merkle tree
     rootHash = hash(merkleTree["merkleRoot"])
-    console.log(merkleTree["merkleRoot"])
     console.log(rootHash)
     contentFileName = "rewards-" + str(chain.id) + "-" + str(merkleTree["merkleRoot"]) + ".json"
     console.log("Saving merkle tree as {}".format(contentFileName))
     with open(contentFileName,"w") as f:
-        json.dump(merkleTree,f)
+        json.dump(merkleTree,f,indent=4)
 
-    badger.badgerTree.proposeRoot(
+
+    farmHarvestedMerkleTree = 0
+    claims = merkleTree["claims"]
+
+    for user, claim in claims.items():
+        if farmTokenAddress in claim["tokens"]:
+            token_index = claim["tokens"].index(farmTokenAddress)
+            amount = claim["cumulativeAmounts"][token_index]
+            console.log("Address {} : {} FARM".format(user,int(float(amount))/1e18))
+            farmHarvestedMerkleTree += int(float(amount))
+            
+    console.log("Total Farm Harvested {}".format(farmHarvestedMerkleTree/1e18))
+    console.log("Difference: {}".format((farmHarvestedMerkleTree/1e18) - (claimsHarvested/1e18)))
+    difference = farmHarvestedMerkleTree - claimsHarvested
+    console.log("Difference: {}".format(farmHarvestedMerkleTree - claimsHarvested))
+    if abs(difference) < 100000 and not test:
+        badger.badgerTree.proposeRoot(
         merkleTree["merkleRoot"],
         rootHash,
         nextCycle,
-        {"from" :badger.keeper,"gas_price":gas_strategy}
-    )
+        {"from" :badger.keeper,"gas_price":gas_strategy})
+        
+
+    
 
 
 
