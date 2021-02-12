74bdae451ed93ecfecfb323e7d845aa4202409ff
Raymond Pulver IV <raymondpulver@my.uri.edu>
2021-02-12 05:54:47 +0000

    fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+require('hardhat-deploy');
+require('hardhat-deploy-ethers');
+
+require('@nomiclabs/hardhat-etherscan');
+
 module.exports = {
   solidity: "0.6.12",
+  networks: {
+    hardhat: {}
+  }
 };
