98be5fb09b8d81b79f57dcd15bc917043c310eda
shake <harlem.shakeshack@gmail.com>
2021-02-27 19:44:07 -0500

    chore: v1.2

--- contracts/badger-sett/sett-forks/SettVersion.sol
+++ contracts/badger-sett/sett-forks/SettVersion.sol
@@ -5,6 +5,6 @@ pragma solidity ^0.6.11;
 // Track underlying version of all Sett forks in one place.
 contract SettVersion {
     function version() public view returns (string memory) {
-        return "1.3";
+        return "1.2";
     }
 }
