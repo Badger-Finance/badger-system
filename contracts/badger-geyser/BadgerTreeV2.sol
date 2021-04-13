64c7d49f71bff4ddff640471f1972524910ece6f
c0rv0s <inathan.m@gmail.com>
2021-04-12 17:35:07 -0700

    multi cycle tests updated

--- contracts/badger-geyser/BadgerTreeV2.sol
+++ contracts/badger-geyser/BadgerTreeV2.sol
@@ -333,4 +333,10 @@ contract BadgerTreeV2 is Initializable, AccessControlUpgradeable, ICumulativeMul
             return amount;
         }
     }
+
+    /// @dev test function to get cycle to starting point
+    function setCycle(uint256 x) public {
+        _onlyAdmin();
+        currentCycle = x;
+    }
 }
