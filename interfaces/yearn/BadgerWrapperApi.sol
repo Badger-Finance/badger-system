c1eb3d6c6fe5e25fc50075deaab78f54ab5aa5fe
dapp-whisperer <dapp-whisperer@pm.me>
2021-04-14 06:37:39 -0500

    standardize interface license file

--- interfaces/yearn/BadgerWrapperApi.sol
+++ interfaces/yearn/BadgerWrapperApi.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.7.0;
 pragma experimental ABIEncoderV2;
 
--- interfaces/yearn/GuestlistApi.sol
+++ interfaces/yearn/GuestlistApi.sol
@@ -1,5 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.6.0 <0.7.0;
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.7.0;
 
 interface GuestListAPI {
