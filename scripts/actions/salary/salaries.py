2b3024bfd20d86a83b5180849a5e3f41759c7b5a
c0rv0s <inathan.m@gmail.com>
2021-04-14 21:31:33 -0700

    use multicall

--- scripts/actions/salary/salaries.py
+++ scripts/actions/salary/salaries.py
@@ -4,28 +4,47 @@ from time import time
 from datetime import datetime
 from dotmap import DotMap
 
+from helpers.multicall.call import Call
+from helpers.multicall.multicall import Multicall
+
 import json
 import brownie
 import os
 
+def __get_data__(logger_address: str, start: int, end: int):
+  calls = [Call(logger_address, ['getEntry(uint256)(address,address,uint256,uint256,uint256)', i], [['recipient_'+str(i), None], ['token_'+str(i), None], ['amount_'+str(i), None], ['startTime_'+str(i), None], ['endTime_'+str(i), None]]) for i in range(start, end)]
+  multi = Multicall(calls)()
+
+  result = []
+  for i in range(start, end):
+    result.append((
+      web3.toChecksumAddress(multi['recipient_'+str(i)]),
+      web3.toChecksumAddress(multi['token_'+str(i)]),
+      multi['amount_'+str(i)],
+      multi['startTime_'+str(i)],
+      multi['endTime_'+str(i)]
+    ))
+  return result
+
 
 def fetch_salaries(manager: str, logger_address: str, test=False):
-  with open("build/contracts/ContributorLogger.json") as f:
+  with open('build/contracts/ContributorLogger.json') as f:
     logger_abi = json.load(f)['abi']
 
-  logger_contract = brownie.Contract.from_abi("ContributorLogger", logger_address, logger_abi)
-
-  now = int(time())
-  salaries = dict()
+  logger_contract = brownie.Contract.from_abi('ContributorLogger', logger_address, logger_abi)
 
   next_id = logger_contract.nextId()
   last_paid_timestamp = logger_contract.lastPaidTimestamp()
   first_paid_index = logger_contract.firstPaidIndex()
 
-  updated_first_paid_index = first_paid_index
+  entries = __get_data__(logger_address, first_paid_index, next_id)
 
-  for i in range(first_paid_index, next_id):
-    (recipient, token, amount, startTime, endTime) = logger_contract.getEntry(i)
+  updated_first_paid_index = first_paid_index
+  salaries = dict()
+  now = int(time())
+
+  for entry in entries:
+    (recipient, token, amount, startTime, endTime) = entry
     
     if startTime <= now <= endTime or last_paid_timestamp <= endTime <= now:
       amount_duration = endTime - startTime
@@ -50,7 +69,7 @@ def fetch_salaries(manager: str, logger_address: str, test=False):
   salary_json_filename = 'salaries-' + date_now + '.json'
   if test:
     salary_json_filename = 'test-' + salary_json_filename
-  salary_json_filename = "salaries/" + salary_json_filename
+  salary_json_filename = 'salaries/' + salary_json_filename
 
   with open(salary_json_filename, 'w') as salaries_dumped :
     json.dump(salaries, salaries_dumped, indent=4)
--- tests/rewards_tree/test_salaries.py
+++ tests/rewards_tree/test_salaries.py
@@ -19,9 +19,9 @@ from scripts.actions.salary.salaries import fetch_salaries
 
 console = Console()
 
-badger_token = "0x3472A5A71965499acd81997a54BBA8D852C6E53d"
+badger_token = '0x3472A5A71965499acd81997a54BBA8D852C6E53d'
 
-@pytest.fixture(scope="function", autouse="True")
+@pytest.fixture(scope='function', autouse='True')
 def setup():
     from assistant.rewards import rewards_assistant
     return rewards_assistant
@@ -37,22 +37,22 @@ def test_salaries(setup):
     loggerContract.initialize(admin, admin, manager)
 
     # salary period ends in the future
-    console.print("Testing a salary period that ends after the check is made")
+    console.print('Testing a salary period that ends after the check is made')
     now = int(time()) + 1
     first_entry = {
-      "recipient": accounts[2].address,
-      "token": badger_token,
-      "amount": 100000,
-      "startTime": now,
-      "endTime": now + 10,
+      'recipient': accounts[2].address,
+      'token': badger_token,
+      'amount': 100000,
+      'startTime': now,
+      'endTime': now + 10,
     }
     loggerContract.createEntry(
-      first_entry["recipient"],
-      first_entry["token"],
-      first_entry["amount"],
-      first_entry["startTime"],
-      first_entry["endTime"],
-      { "from": manager }
+      first_entry['recipient'],
+      first_entry['token'],
+      first_entry['amount'],
+      first_entry['startTime'],
+      first_entry['endTime'],
+      { 'from': manager }
     )
 
     wait_time = 5
@@ -65,17 +65,17 @@ def test_salaries(setup):
 
     # depending on when the block gets mined it could be plus or minus one second
     payouts = [
-      int((wait_time) * (first_entry["amount"] // (first_entry["endTime"] - first_entry["startTime"]))),
-      int((wait_time + 1) * (first_entry["amount"] // (first_entry["endTime"] - first_entry["startTime"]))),
-      int((wait_time - 1) * (first_entry["amount"] // (first_entry["endTime"] - first_entry["startTime"])))
+      int((wait_time) * (first_entry['amount'] // (first_entry['endTime'] - first_entry['startTime']))),
+      int((wait_time + 1) * (first_entry['amount'] // (first_entry['endTime'] - first_entry['startTime']))),
+      int((wait_time - 1) * (first_entry['amount'] // (first_entry['endTime'] - first_entry['startTime'])))
     ]
-    expected_value1 = salary_json[first_entry["recipient"]][first_entry["token"]]
+    expected_value1 = salary_json[first_entry['recipient']][first_entry['token']]
     assert (expected_value1 == payouts[0] or expected_value1 == payouts[1] or expected_value1 == payouts[2])
 
     sleep(wait_time + 1)
 
     # checking after salary time ended
-    console.print("Testing that salary is correct after full period has elapsed")
+    console.print('Testing that salary is correct after full period has elapsed')
     salary_json_filename = fetch_salaries(manager, loggerContract.address, True)
 
     with open(salary_json_filename) as f:
@@ -83,47 +83,47 @@ def test_salaries(setup):
 
     # depending on when the block gets mined it could be one second off
     payouts = [
-      int((wait_time) * (first_entry["amount"] // (first_entry["endTime"] - first_entry["startTime"]))),
-      int((wait_time + 1) * (first_entry["amount"] // (first_entry["endTime"] - first_entry["startTime"]))),
-      int((wait_time - 1) * (first_entry["amount"] // (first_entry["endTime"] - first_entry["startTime"])))
+      int((wait_time) * (first_entry['amount'] // (first_entry['endTime'] - first_entry['startTime']))),
+      int((wait_time + 1) * (first_entry['amount'] // (first_entry['endTime'] - first_entry['startTime']))),
+      int((wait_time - 1) * (first_entry['amount'] // (first_entry['endTime'] - first_entry['startTime'])))
     ]
-    expected_value2 = salary_json[first_entry["recipient"]][first_entry["token"]]
+    expected_value2 = salary_json[first_entry['recipient']][first_entry['token']]
     assert (expected_value2 == payouts[0] or expected_value2 == payouts[1] or expected_value2 == payouts[2])
-    assert expected_value1 + expected_value2 == first_entry["amount"]
+    assert expected_value1 + expected_value2 == first_entry['amount']
 
     # multiple salaries in the same time period
-    console.print("Testing two salaries overlapping each other")
+    console.print('Testing two salaries overlapping each other')
     now1 = int(time()) + 1
     first_entry = {
-      "recipient": accounts[2].address,
-      "token": badger_token,
-      "amount": 100000,
-      "startTime": now1,
-      "endTime": now1 + 5,
+      'recipient': accounts[2].address,
+      'token': badger_token,
+      'amount': 100000,
+      'startTime': now1,
+      'endTime': now1 + 5,
     }
     loggerContract.createEntry(
-      first_entry["recipient"],
-      first_entry["token"],
-      first_entry["amount"],
-      first_entry["startTime"],
-      first_entry["endTime"],
-      { "from": manager }
+      first_entry['recipient'],
+      first_entry['token'],
+      first_entry['amount'],
+      first_entry['startTime'],
+      first_entry['endTime'],
+      { 'from': manager }
     )
     now2 = int(time()) + 1
     second_entry = {
-      "recipient": accounts[3].address,
-      "token": badger_token,
-      "amount": 1580945,
-      "startTime": now2,
-      "endTime": now2 + 10,
+      'recipient': accounts[3].address,
+      'token': badger_token,
+      'amount': 650,
+      'startTime': now2,
+      'endTime': now2 + 10,
     }
     loggerContract.createEntry(
-      second_entry["recipient"],
-      second_entry["token"],
-      second_entry["amount"],
-      second_entry["startTime"],
-      second_entry["endTime"],
-      { "from": manager }
+      second_entry['recipient'],
+      second_entry['token'],
+      second_entry['amount'],
+      second_entry['startTime'],
+      second_entry['endTime'],
+      { 'from': manager }
     )
 
     wait_time = 8
@@ -134,37 +134,38 @@ def test_salaries(setup):
     with open(salary_json_filename) as f:
       salary_json = json.load(f)
 
-    # should have received entire payout
-    assert salary_json[first_entry["recipient"]][first_entry["token"]] == first_entry["amount"]
+    # first address should have received entire payout
+    assert salary_json[first_entry['recipient']][first_entry['token']] == first_entry['amount']
 
-    # should have received a partial payout (may be off by +/- 1 second)
+    # second address should have received a partial payout (may be off by +/- 1 second)
     payouts = [
-      int((wait_time) * (second_entry["amount"] // (second_entry["endTime"] - second_entry["startTime"]))),
-      int((wait_time + 1) * (second_entry["amount"] // (second_entry["endTime"] - second_entry["startTime"]))),
-      int((wait_time - 1) * (second_entry["amount"] // (second_entry["endTime"] - second_entry["startTime"])))
+      int((wait_time) * (second_entry['amount'] // (second_entry['endTime'] - second_entry['startTime']))),
+      int((wait_time + 1) * (second_entry['amount'] // (second_entry['endTime'] - second_entry['startTime']))),
+      int((wait_time - 1) * (second_entry['amount'] // (second_entry['endTime'] - second_entry['startTime'])))
     ]
-    expected_value = salary_json[second_entry["recipient"]][second_entry["token"]]
-    assert (expected_value == payouts[0] or expected_value == payouts[1] or expected_value == payouts[2])
+    expected_value1 = salary_json[second_entry['recipient']][second_entry['token']]
+    assert (expected_value1 == payouts[0] or expected_value1 == payouts[1] or expected_value1 == payouts[2])
 
     # after 5 more seconds there should only be one salary entry
-    console.print("Testing after the first period has ended")
-    last_paid_timestamp1 = loggerContract.lastPaidTimestamp()
+    console.print('Testing after the first period has ended')
+    last_paid_timestamp = loggerContract.lastPaidTimestamp()
     wait_time = 5
     sleep(wait_time)
 
     salary_json_filename = fetch_salaries(manager, loggerContract.address, True)
 
-    duration = second_entry["endTime"] - last_paid_timestamp1
+    duration = second_entry['endTime'] - last_paid_timestamp
     with open(salary_json_filename) as f:
       salary_json = json.load(f)
 
     # should have received a partial payout
-    expected_value = salary_json[second_entry["recipient"]][second_entry["token"]]
-    payout = int((duration) * (second_entry["amount"] // (second_entry["endTime"] - second_entry["startTime"])))
-    assert expected_value == payout
+    expected_value2 = salary_json[second_entry['recipient']][second_entry['token']]
+    payout = int((duration) * (second_entry['amount'] // (second_entry['endTime'] - second_entry['startTime'])))
+    assert expected_value2 == payout
+    assert expected_value1 + expected_value2 == second_entry['amount']
 
     # should be no more entries when called again
-    console.print("Testing with no more salaries to be paid out")
+    console.print('Testing with no more salaries to be paid out')
     wait_time = 1
     sleep(wait_time)
 
