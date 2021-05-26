a34af0c0945a3ad83280081b2871180451c57973
shakeshack <harlem.shakeshack@gmail.com>
2021-05-26 10:28:46 +0700

    feat: add meta token

--- tests/matic/test_meta_token.py
+++ tests/matic/test_meta_token.py
@@ -2,22 +2,40 @@ from brownie import (
     accounts,
     UpgradeableChildERC20,
     UpgradableProxy,
+    UpgradeableMetaToken,
 )
 
 from helpers.registry import registry
 from helpers.constants import AddressZero
+from helpers.proxy_utils import deploy_proxy_admin, deploy_proxy
 
 
 def test_meta_token():
     gov = accounts[3]
+
+    tokenName = "Interest Bearing Bitcoin"
+    tokenSymbol = "ibBTC"
+    decimals = 8
+
+    proxyAdmin = deploy_proxy_admin(gov)
+    metaTokenLogic = UpgradeableMetaToken.deploy({"from": gov})
+    metaToken = deploy_proxy(
+        "MetaTokenIbBTC",
+        UpgradeableMetaToken.abi,
+        proxyAdmin.address,
+        metaTokenLogic.initialize.encode_input(
+            tokenName,
+            tokenSymbol,
+            decimals,
+        ),
+    )
+
     childLogic = UpgradeableChildERC20.deploy({"from": gov})
     childProxy = UpgradableProxy.deploy(AddressZero, {"from": gov})
     childProxy.updateAndCall(childLogic, childLogic.initialize.encode_input(
-        "Interest Bearing Bitcoin",
-        "ibBTC",
-        8,
+        f"{tokenName} - matic bridge",
+        f"{tokenSymbol}-matic",
+        decimals,
         registry.matic.childChainManager,
-        # TODO: Impl meta token contract and validate test that
-        # proxied deposits/withdraws work from child chain manager.
-        AddressZero,
+        metaToken,
     ))
