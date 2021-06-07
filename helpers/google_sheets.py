fc869673b5dc0b373e978e9de04f8e7a0c46a98c
Nate <inathan.m@gmail.com>
2021-06-07 15:01:07 -0700

    extra line

--- helpers/google_sheets.py
+++ helpers/google_sheets.py
@@ -6,7 +6,6 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from rich.console import Console
-from typing import Union
 
 console = Console()
 
