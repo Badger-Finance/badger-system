72df2d3b814f034a70414e305fa423201d7734b6
Nate <inathan.m@gmail.com>
2021-06-08 17:08:16 -0700

    fix: gas intelligence

--- scripts/view/gas_intelligence.py
+++ scripts/view/gas_intelligence.py
@@ -6,7 +6,7 @@ import os
 from time import time
 from dotmap import DotMap
 
-'''
+"""
 Find the cheapest gas price that could be expected to get mined in a reasonable amount of time.
 Historical query returns average gas price for each of the specified 
 time units (minutes or hours). 
@@ -31,22 +31,24 @@ Returns:
 Test:
 ----------------
 test()
-'''
+"""
 
-HISTORICAL_URL = 'https://api.anyblock.tools/ethereum/ethereum/mainnet/es/'
-CREDENTIALS = "/../../credentials.json"
-BINS = 60 # number of bins for histogram
+HISTORICAL_URL = "https://api.anyblock.tools/ethereum/ethereum/mainnet/es/"
+CREDENTIALS = "/../../any-block-credentials.json"
+BINS = 60  # number of bins for histogram
 
 # convert wei to gwei
 def to_gwei(x: float) -> float:
-    return x / 10**9
+    return x / 10 ** 9
 
 
 # Initialize the ElasticSearch Client
 def initialize_elastic(network: str) -> any:
     # Api access credentials from https://www.anyblockanalytics.com/
     AUTH = json.load(open(os.path.dirname(__file__) + CREDENTIALS))
-    return Elasticsearch(hosts=[network], http_auth=(AUTH['email'], AUTH['key']), timeout=180)
+    return Elasticsearch(
+        hosts=[network], http_auth=(AUTH["email"], AUTH["key"]), timeout=180
+    )
 
 
 # fetch average hourly gas prices over the last specified hours
@@ -54,39 +56,35 @@ def fetch_gas_hour(network: str, hours=24) -> list[float]:
     es = initialize_elastic(network)
     now = int(time())
     seconds = hours * 3600
-    data = es.search(index='tx', doc_type='tx', body = {
-        "_source":["timestamp","gasPrice.num"],
-        "query": {
-        "bool": {
-            "must": [{ 
-                "range":{
-                    "timestamp": {
-                        "gte": now - seconds,
-                        "lte": now
-                    }
-                }}
-                
-            ]
-        }
-        },
-        "aggs": {
-        "hour_bucket": {
-            "date_histogram": {
-                "field": "timestamp",
-                "interval": "1H",
-                "format": "yyyy-MM-dd hh:mm:ss"
+    data = es.search(
+        index="tx",
+        doc_type="tx",
+        body={
+            "_source": ["timestamp", "gasPrice.num"],
+            "query": {
+                "bool": {
+                    "must": [
+                        {"range": {"timestamp": {"gte": now - seconds, "lte": now}}}
+                    ]
+                }
             },
             "aggs": {
-                "avgGasHour": {
-                    "avg": {
-                        "field": "gasPrice.num"
-                    }
+                "hour_bucket": {
+                    "date_histogram": {
+                        "field": "timestamp",
+                        "interval": "1H",
+                        "format": "yyyy-MM-dd hh:mm:ss",
+                    },
+                    "aggs": {"avgGasHour": {"avg": {"field": "gasPrice.num"}}},
                 }
-            }
-        }
-    }
-    })
-    return [ x['avgGasHour']['value'] for x in data['aggregations']['hour_bucket']['buckets'] if x['avgGasHour']['value']]
+            },
+        },
+    )
+    return [
+        x["avgGasHour"]["value"]
+        for x in data["aggregations"]["hour_bucket"]["buckets"]
+        if x["avgGasHour"]["value"]
+    ]
 
 
 # fetch average gas prices per minute over the last specified minutes
@@ -94,42 +92,40 @@ def fetch_gas_min(network: str, minutes=60) -> list[float]:
     es = initialize_elastic(network)
     now = int(time())
     seconds = minutes * 60
-    data = es.search(index='tx', doc_type='tx', body = {
-        "_source":["timestamp","gasPrice.num"],
-        "query": {
-        "bool": {
-            "must": [{
-                "range":{
-                    "timestamp": {
-                        "gte": now - seconds,
-                        "lte": now
-                    }
-                }}
-                
-            ]
-        }
-        },
-        "aggs": {
-        "minute_bucket": {
-            "date_histogram": {
-                "field": "timestamp",
-                "interval": "1m",
-                "format": "yyyy-MM-dd hh:mm"
+    data = es.search(
+        index="tx",
+        doc_type="tx",
+        body={
+            "_source": ["timestamp", "gasPrice.num"],
+            "query": {
+                "bool": {
+                    "must": [
+                        {"range": {"timestamp": {"gte": now - seconds, "lte": now}}}
+                    ]
+                }
             },
             "aggs": {
-                "avgGasMin": {
-                    "avg": {
-                        "field": "gasPrice.num"
-                    }
-                }}}
-    }
-    })
-    return [ x['avgGasMin']['value'] for x in data['aggregations']['minute_bucket']['buckets'] if x['avgGasMin']['value']]
+                "minute_bucket": {
+                    "date_histogram": {
+                        "field": "timestamp",
+                        "interval": "1m",
+                        "format": "yyyy-MM-dd hh:mm",
+                    },
+                    "aggs": {"avgGasMin": {"avg": {"field": "gasPrice.num"}}},
+                }
+            },
+        },
+    )
+    return [
+        x["avgGasMin"]["value"]
+        for x in data["aggregations"]["minute_bucket"]["buckets"]
+        if x["avgGasMin"]["value"]
+    ]
 
 
 def is_outlier(points: list[float], thresh=3.5) -> list[bool]:
     """
-    Returns a boolean array with True if points are outliers and False 
+    Returns a boolean array with True if points are outliers and False
     otherwise.
 
     Parameters:
@@ -147,39 +143,44 @@ def is_outlier(points: list[float], thresh=3.5) -> list[bool]:
     ----------
         Boris Iglewicz and David Hoaglin (1993), "Volume 16: How to Detect and
         Handle Outliers", The ASQC Basic References in Quality Control:
-        Statistical Techniques, Edward F. Mykytka, Ph.D., Editor. 
+        Statistical Techniques, Edward F. Mykytka, Ph.D., Editor.
     """
     if len(points.shape) == 1:
-        points = points[:,None]
+        points = points[:, None]
     median = np.median(points, axis=0)
-    diff = np.sum((points - median)**2, axis=-1)
+    diff = np.sum((points - median) ** 2, axis=-1)
     diff = np.sqrt(diff)
     med_abs_deviation = np.median(diff)
 
     modified_z_score = 0.6745 * diff / med_abs_deviation
-    
+
     return modified_z_score > thresh
 
 
 # main entry point
-def analyze_gas(options={ 'timeframe': 'minutes', 'periods': 60 }) -> tuple[int, int, int]:
+def analyze_gas(
+    options={"timeframe": "minutes", "periods": 60}
+) -> tuple[int, int, int]:
     if not os.path.isfile(os.path.dirname(__file__) + CREDENTIALS):
-        raise ValueError("No credentials for historical gas analysis found")
+        print("Could not fetch historical gas data")
+        return DotMap(
+            mode=999999999999999999, median=999999999999999999, std=999999999999999999
+        )
 
     gas_data = []
 
     # fetch data
-    if options['timeframe'] == 'minutes':
-        gas_data = fetch_gas_min(HISTORICAL_URL, options['periods'])
+    if options["timeframe"] == "minutes":
+        gas_data = fetch_gas_min(HISTORICAL_URL, options["periods"])
     else:
-        gas_data = fetch_gas_hour(HISTORICAL_URL, options['periods'])
+        gas_data = fetch_gas_hour(HISTORICAL_URL, options["periods"])
     gas_data = np.array(gas_data)
 
     # remove outliers
     filtered_gas_data = gas_data[~is_outlier(gas_data)]
 
-    # Create histogram 
-    counts, bins = np.histogram(filtered_gas_data, bins = BINS)
+    # Create histogram
+    counts, bins = np.histogram(filtered_gas_data, bins=BINS)
 
     # Find most common gas price
     biggest_bin = 0
@@ -193,9 +194,7 @@ def analyze_gas(options={ 'timeframe': 'minutes', 'periods': 60 }) -> tuple[int,
     if int(midpoint) == 0:
         print("Could not fetch historical gas data")
         return DotMap(
-            mode=999999999999999999,
-            median=999999999999999999,
-            std=999999999999999999
+            mode=999999999999999999, median=999999999999999999, std=999999999999999999
         )
 
     # standard deviation
@@ -204,18 +203,14 @@ def analyze_gas(options={ 'timeframe': 'minutes', 'periods': 60 }) -> tuple[int,
     # average
     median = np.median(filtered_gas_data, axis=0)
 
-    return DotMap(
-        mode=int(midpoint),
-        median=int(median),
-        std=int(standard_dev)
-    )
+    return DotMap(mode=int(midpoint), median=int(median), std=int(standard_dev))
 
 
 # run this to test analyze_gas and print values
 def main() -> tuple[int, int, int]:
     results = analyze_gas()
-    print('timeframe:', 'minutes')
-    print('approximate most common gas price:', to_gwei(results['mode']))
-    print('average gas price:', to_gwei(results['median']))
-    print('standard deviation:', to_gwei(results['std']))
+    print("timeframe:", "minutes")
+    print("approximate most common gas price:", to_gwei(results["mode"]))
+    print("average gas price:", to_gwei(results["median"]))
+    print("standard deviation:", to_gwei(results["std"]))
     return results
