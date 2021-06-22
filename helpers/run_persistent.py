11dceb52a0d7353eae36605c404eb44da7bd94d6
dapp-whisperer <dapp-whisperer@pm.me>
2021-06-22 14:22:09 -0500

    full error printout first run_persistent

--- helpers/run_persistent.py
+++ helpers/run_persistent.py
@@ -4,6 +4,11 @@ from helpers.console_utils import console
 
 
 def run_persistent(function, args, num_args=1, run_interval=10):
+
+    # Full error printout on first run
+    function(args[0], args[1])
+    time.sleep(run_interval)
+    
     while True:
         try:
             function(args[0], args[1])
