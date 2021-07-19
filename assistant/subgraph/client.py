068a94d94c96b8ef7150bea394a79a1c2f66fa82
Darruma <umerderr@gmail.com>
2021-07-19 16:39:20 +0100

    feat: add nft subgraph id

--- assistant/subgraph/client.py
+++ assistant/subgraph/client.py
@@ -14,6 +14,7 @@ tokens_client = make_gql_client("tokens")
 sett_client = make_gql_client("setts")
 harvests_client = make_gql_client("harvests")
 
+
 def fetch_tree_distributions(startBlock, endBlock):
     query = gql(
         """
--- assistant/subgraph/config.py
+++ assistant/subgraph/config.py
@@ -1,6 +1,6 @@
 subgraph_ids = {
-    "tokens": "0xba5edb751ccf93770796e273d8bce83e1e81e2d4-4",
     "setts": "0xba5edb751ccf93770796e273d8bce83e1e81e2d4-5",
     "harvests": "0xba5edb751ccf93770796e273d8bce83e1e81e2d4-2",
     "tokens": "0xba5edb751ccf93770796e273d8bce83e1e81e2d4-4",
+    "nfts": "0xba5edb751ccf93770796e273d8bce83e1e81e2d4-3",
 }
