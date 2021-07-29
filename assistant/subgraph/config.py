817bb2fc71cc1292c88afa0751b1cf06408c99b7
Darruma <umerderr@gmail.com>
2021-07-29 17:23:44 +0100

    feat: readd tokens into boost and add harvest and nfts subgraph from bgraph

--- assistant/subgraph/config.py
+++ assistant/subgraph/config.py
@@ -1,8 +1,8 @@
 subgraph_ids = {
-    "harvests": "0xba5edb751ccf93770796e273d8bce83e1e81e2d4-2",
-    "nfts": "0xba5edb751ccf93770796e273d8bce83e1e81e2d4-3",
 }
 subgraph_urls = {
     "setts": "https://bgraph-eth.badger.guru/subgraphs/name/swole/setts",
     "tokens": "https://bgraph-eth.badger.guru/subgraphs/name/swole/tokens",
+    "harvests": "https://bgraph-eth.badger.guru/subgraphs/name/swole/tree-rewards",
+    "nfts": "https://bgraph-eth.badger.guru/subgraphs/name/darruma/badger-nfts"
 }
