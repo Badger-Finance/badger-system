0242ad5cb1a0fd1f340f2f6eeb84de7ee4f4ee3c
sajanrajdev <sajanrajdev10@gmail.com>
2021-09-02 16:52:03 -0400

    fix: fixed script and added eth logic json for testing

--- scripts/multichain/deploy_chain_infrastructure.py
+++ scripts/multichain/deploy_chain_infrastructure.py
@@ -2,20 +2,7 @@ from dotmap import DotMap
 import os
 import json
 import time
-from brownie import (
-    accounts,
-    network,
-    SettV3,
-    AdminUpgradeabilityProxy,
-    Controller,
-    BadgerRegistry,
-    BadgerTreeV2,
-    KeeperAccessControl,
-    RewardsLogger,
-    ProxyAdmin,
-    VipCappedGuestListBbtcUpgradeable,
-    WarRoomGatedProxy
-)
+from brownie import AdminUpgradeabilityProxy
 from scripts.connect_account import connect_account
 from helpers.sett.strategy_registry import contract_name_to_artifact
 from helpers.constants import AddressZero
@@ -39,17 +26,40 @@ contractNames = [
 ]
 
 def main():
-    path = os.getcwd() + "/arbirtum_logic.json"
+    """
+    Deploys all the required infrastructure to get started on a new chain:
+
+    1. Fetches required logic contracts from JSON file. If the logic for a contract
+    is missing, it deploys it. It will do this for all contracts whose names are 
+    contained on teh `contractNames` array.
+
+    2. Deploys all unique contracts under AdminUpgradabilityProxies and initializes
+    them with the deployer address as governance and any missing actor.
+
+    3. Adds all deployed contracts to the registry with its designated key.
+    """
+
+    # Set path to JSON file with the logic contracts' addresses keyed by contract name
+    path = os.getcwd() + "/scripts/multichain/eth_logic.json"
     with open(path) as f:
         data = json.load(f)
 
     proxyAdminDev = data["proxyAdminDev"]
+    proxyAdminTimelock = data["proxyAdminTimelock"]
 
     # Connect to deployer account from local Keystore
     dev = connect_account()
 
+    # =========================== #
+    #         Deploy Logic        #
+    # =========================== #
+
     console.print("[blue]Fetching or deploying logic...[/blue]")
     logic = deploy_logic(dev, data, contractNames)
+
+    # =========================== #
+    #       Deploy Proxies        #
+    # =========================== #
 
     console.print("[blue]Deploying proxies...[/blue]")
 
@@ -123,6 +133,21 @@ def main():
         args
     )
 
+    # =========================== #
+    #         Set Registry        #
+    # =========================== #
+
+    console.print("[blue]Setting keys on registry...[/blue]")
+
+    registry.set("keeperAccessControl", keeper.address, {"from": dev})
+    registry.set("keeper", keeper.address, {"from": dev})
+    registry.set("rewardsLogger", logger.address, {"from": dev})
+    registry.set("controller", controller.address, {"from": dev})
+    registry.set("badgerTree", tree.address, {"from": dev})
+    registry.set("guardian", guardian.address, {"from": dev})
+    registry.set("proxyAdminDev", proxyAdminDev, {"from": dev})
+    registry.set("proxyAdminTimelock", proxyAdminTimelock, {"from": dev})
+
 def deploy_logic(dev, data, contractNames):
     logic = DotMap()
 
@@ -130,7 +155,6 @@ def deploy_logic(dev, data, contractNames):
         if name not in data:
             artifact = contract_name_to_artifact(name)
             contractLogic = artifact.deploy({"from": dev})
-            data.append({name: contractLogic.address})
             logic[name] = contractLogic
             console.print(name, "[green]logic deployed![/green]")
         else:
@@ -151,7 +175,7 @@ def deploy_proxyContract(dev, proxyAdmin, logic, name, args):
     )
     time.sleep(sleep_between_tx)
 
-    console.print(name, "[green]proxy deployed![/green]")
+    console.print(name, "[green]proxy deployed at[/green]", proxy.address)
 
     ## We delete from deploy and then fetch again so we can interact
     artifact = contract_name_to_artifact(name)
