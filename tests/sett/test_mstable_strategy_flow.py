f106b019dde1828dae255212f5963a1a35285dac
sajanrajdev <sajanrajdev10@gmail.com>
2021-09-04 15:52:51 -0400

    test: add more day to 6 month sleep

--- tests/sett/test_mstable_strategy_flow.py
+++ tests/sett/test_mstable_strategy_flow.py
@@ -117,7 +117,7 @@ def test_single_user_harvest_flow(settConfig):
     snap.settWithdraw(depositAmount // 2, {"from": deployer})
 
     # Chain sleeps for more than 6 months to allow for a mta full vesting cycle
-    chain.sleep(days(186))
+    chain.sleep(days(187))
     chain.mine()
 
     # Harvest
